be7ddab3de79359a2823674cec224cf39982d0a4
davidvignon <davidvignon86@yahoo.fr>
2023-09-14 16:07:47 +0200

    correction DAY5 part2

--- src/main/kotlin/year2015/Day05.kt
+++ src/main/kotlin/year2015/Day05.kt
@@ -56,42 +56,81 @@ class Day05 {
     private fun partTwo(lines: List<String>) {
         println(
             lines.count { line: String ->
+                // Rule 1: Check for pairs of characters that appear at least twice without overlapping.
+                val hasPairTwice = hasRepeatingPair(line)
 
-                val pairsWithIndex = line.mapIndexedNotNull { index, char ->
-                    line.getOrNull(index + 1)?.let { next ->
-                        PairWithIndex(
-                            index = index,
-                            firstChar = char,
-                            secondChar = next
-                        )
-                    }
-                }
+                // Rule 2: Check for a letter that repeats with exactly one letter between them.
+                val hasRepeatingLetterWithOneBetween = hasRepeatingLetterWithOneBetween(line)
 
-                if (
-                    pairsWithIndex.any { pair ->
-                        pairsWithIndex.find { possibleMatchPair ->
-                            pair.firstChar == possibleMatchPair.firstChar
-                                && pair.secondChar == possibleMatchPair.secondChar
-                                && pair.index + 2 < possibleMatchPair.index
-                        } != null
-                    }
-                ) {
-
-                    val groupOfThree = line.windowed(3, 1)
-                    for (i in groupOfThree) {
-                        if (i.first() == i.last()) {
-                            return@count true
-                        }
-                    }
-                }
-                false
+                // Both rules must be satisfied for the string to be nice.
+                hasPairTwice && hasRepeatingLetterWithOneBetween
             }
         )
     }
 
-    data class PairWithIndex(
-        val index: Int,
-        val firstChar: Char,
-        val secondChar: Char
-    )
+    private fun hasRepeatingPair(line: String): Boolean {
+        val pairs = mutableSetOf<String>()
+        for (i in 0 until line.length - 1) {
+            val pair = line.substring(i, i + 2)
+            if (pairs.contains(pair)) {
+                return true
+            }
+            if (line.indexOf(pair, i + 2) != -1) {
+                pairs.add(pair)
+            }
+        }
+        return false
+    }
+
+    private fun hasRepeatingLetterWithOneBetween(line: String): Boolean {
+        for (i in 0 until line.length - 2) {
+            if (line[i] == line[i + 2]) {
+                return true
+            }
+        }
+        return false
+    }
 }
+
+//    private fun partTwo(lines: List<String>) {
+//        println(
+//            lines.count { line: String ->
+//
+//                val pairsWithIndex = line.mapIndexedNotNull { index, char ->
+//                    line.getOrNull(index + 1)?.let { next ->
+//                        PairWithIndex(
+//                            index = index,
+//                            firstChar = char,
+//                            secondChar = next
+//                        )
+//                    }
+//                }
+//
+//                if (
+//                    pairsWithIndex.any { pair ->
+//                        pairsWithIndex.find { possibleMatchPair ->
+//                            pair.firstChar == possibleMatchPair.firstChar
+//                                && pair.secondChar == possibleMatchPair.secondChar
+//                                && pair.index + 2 < possibleMatchPair.index
+//                        } != null
+//                    }
+//                ) {
+//
+//                    val groupOfThree = line.windowed(3, 1)
+//                    for (i in groupOfThree) {
+//                        if (i.first() == i.last()) {
+//                            return@count true
+//                        }
+//                    }
+//                }
+//                false
+//            }
+//        )
+//    }
+//
+//    data class PairWithIndex(
+//        val index: Int,
+//        val firstChar: Char,
+//        val secondChar: Char
+//    )
+//}
