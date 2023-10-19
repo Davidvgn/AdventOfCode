7b9425f41eb682bdedb169a7c55e723773e72692
davidvignon <davidvignon86@yahoo.fr>
2023-10-19 13:10:20 +0200

    2016 day2 wrong

--- src/main/kotlin/year2016/Day02.kt
+++ src/main/kotlin/year2016/Day02.kt
@@ -25,30 +25,78 @@ class Day02 {
 
     private fun partOne(lines: List<String>) {
 
+        val leftEdge: List<Int> = listOf(1, 4, 7)
+        val rightEdge: List<Int> = listOf(3, 6, 9)
+        val upEdge: List<Int> = listOf(1, 2, 3)
+        val downEdge: List<Int> = listOf(7, 8, 9)
+
+        val result: MutableList<Int> = mutableListOf()
 
         lines.forEach {
-            println(
+            result.add(
                 it.fold(5) { acc: Int, s: Char ->
-                    acc + if (s == 'L' && acc != 1 && acc != 4 && acc != 7) {
+                    acc + if (s == 'L' && !leftEdge.contains(acc)) {
                         (-1)
-                    } else if (s == 'R' && acc != 3 && acc != 6 && acc != 9) {
+                    } else if (s == 'R' && !rightEdge.contains(acc)) {
                         1
-                    } else if (s == 'U' && acc != 1 && acc != 2 && acc != 3) {
+                    } else if (s == 'U' && !upEdge.contains(acc)) {
                         (-3)
-
-                    } else if (s == 'D' && acc != 7 && acc != 8 && acc != 9) {
-                        +3
+                    } else if (s == 'D' && !downEdge.contains(acc)) {
+                        3
                     } else {
                         0
                     }
                 }
             )
         }
-
+        println(result)
     }
 
 
     private fun partTwo(lines: List<String>) {
-        println(lines.size)
+
+        val leftEdge: List<Int> = listOf(13, 10, 5, 2, 1)
+        val rightEdge: List<Int> = listOf(1, 4, 9, 12, 13)
+        val upEdge: List<Int> = listOf(5, 2, 1, 4, 9)
+        val downEdge: List<Int> = listOf(5, 10, 13, 12, 9)
+
+        val result: MutableList<Int> = mutableListOf()
+        val resultStrg: MutableList<String> = mutableListOf()
+
+        lines.forEach { index ->
+            result.add(index.fold(5) { acc: Int, s: Char ->
+                acc + if (s == 'L' && !leftEdge.contains(acc)) {
+                    (-1)
+                } else if (s == 'R' && !rightEdge.contains(acc)) {
+                    1
+                } else if (s == 'U' && !upEdge.contains(acc) && acc != 3) {
+                    (-4)
+                } else if (s == 'U' && !upEdge.contains(acc) && acc == 3) {
+                    (-2)
+                } else if (s == 'D' && !downEdge.contains(acc) && acc != 11) {
+                    4
+                } else if (s == 'D' && !downEdge.contains(acc) && acc == 11) {
+                    2
+                } else {
+                    0
+                }
+            }
+            )
+
+        }
+
+        for (element in result) {
+            var elementInStrg = element.toString()
+            when (elementInStrg) {
+                "10" -> elementInStrg = "A"
+                "11" -> elementInStrg = "B"
+                "12" -> elementInStrg = "C"
+                "13" -> elementInStrg = "D"
+            }
+            resultStrg.add(elementInStrg)
+
+        }
+        println(resultStrg)
+
     }
 }
